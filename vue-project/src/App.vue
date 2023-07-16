49d37d8f6fc87d619ddec0b1b7e528b185399418
Bạch Hoàng Long <longbh@d-soft.com.vn>
2023-07-16 14:50:57 +0700

    add frontend

---
+++ vue-project/src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <div class="logo">
+            <img src="src\components\icons\logo.png" alt="">
+          </div>    
+          <div class="lang">
+            <!-- <label for="language">Language:</label> -->
+            <select id="language">
+              <option value="en">EN</option>
+              <option value="vn">VN</option>
+            </select>
+        </div>
+        <div class="user">
+          <img src="https://via.placeholder.com/50x50.png?text=User" alt="User">
+        </div>
+      </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+<!-- 
+  <footer>
+    <div class="container">
+      <p>&copy; All rights reserved.</p>
+    </div>
+  </footer> -->
+</template>
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: flex-end;
+    background-color: rgb(254, 254, 252);
+    padding: 16px 0;
+    border-bottom: 1px solid #c9c8c8;
+  }
+
+  .lang {
+    display: flex;
+    margin-right: 10px;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+  }
+
+  .logo {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-grow: 1;
+    /* width: 5px; */
+  }
+
+  .logo img {
+    width: 70px;
+    height: 35px;
+  }
+
+  .user {
+    margin-right: 20px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  footer {
+    background-color: #f1f1f1;
+    padding: 20px;
+    text-align: center;
+    font-size: 14px;
+    color: #888;
+    display: flex;
+    align-self: flex-end;
+  }
+
+  footer .container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  footer .container p {
+    margin: 0;
+  }
+
+</style>
---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="search-bar">
+    
+    <form>
+      <div class="search-wrapper">
+        <input type="text" placeholder="Search..." @input="handleInput" ref="input">
+        <div class="image-upload" @dragover="handleDragOver" @drop="handleDrop">
+          <label for="file-upload"><i class="fa fa-cloud-upload"></i> Upload Image</label>
+          <input id="file-upload" type="file" @change="handleUpload" ref="file"/>
+        </div>
+      </div>
+      <button type="submit" @click.prevent="handleSubmit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleInput(event) {
+        console.log("Input changed:", event.target.value);
+      },
+      handleDragOver(event) {
+        event.preventDefault();
+        console.log("Dragging over...");
+      },
+      handleDrop(event) {
+        event.preventDefault();
+        const file = event.dataTransfer.files[0];
+        console.log("Dropped file:", file.name);
+      },
+      handleUpload(event) {
+        const file = event.target.files[0];
+        console.log("Uploaded file:", file, file.name);
+      },
+      async handleSubmit() {
+        const input = this.$refs.input.value;
+        const file = this.$refs.file.files[0];
+
+        if (input && !file) {
+          const response = await fetch('http://localhost:8000/search-by-word/', {
+            method: 'POST',
+            body: JSON.stringify({text: input}),
+            headers: {'Content-Type': 'application/json'}
+          });
+          const data = await response.json();
+          console.log('Search result:', data);
+        } else if (file && !input) {
+          const formData = new FormData();
+          formData.append('image', file, file.name);
+          // formData.append('filename', file.name);
+          // this.uploadService.imageUpload(formData).subscribe((res) => {
+          //           console.log('image upload success', res);
+          //         });
+          const response = await fetch('http://localhost:8000/search-by-image/', {
+            method: 'POST',
+            body: formData
+          });
+          const data = await response.json();
+          console.log('Search result:', data);
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+  .logo {
+    margin-bottom: 20px;
+  }
+
+  .search-bar {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1000px;
+    border-radius: 30px;
+    border: 3px solid #ccc;
+    padding: 10px;
+  }
+
+  .logo-search {
+    position: absolute;
+    left: 10px;
+  }
+
+  .logo-search img {
+    height: 30px;
+  }
+
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    border-radius: 30px;
+  }
+
+  input[type="text"] {
+    flex: 1;
+    padding: 10px;
+    border: none;
+    border-radius: 30px 0 0 30px;
+    font-size: 18px;
+    background-color: transparent;
+    margin-right: -5px;
+    width:900px;
+  }
+
+  button[type="submit"] {
+    padding: 20px;
+    background-color: #4285f4;
+    color: white;
+    border: none;
+    border-radius: 30px;
+    font-size: 18px;
+    cursor: pointer;
+  }
+
+
+</style>
